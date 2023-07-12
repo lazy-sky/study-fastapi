e876880c0bb95d7ec4aabbb359372a3e7f7632dc
lazy-sky <1cielo@naver.com>
2023-07-12 17:57:48 +0900

    Request Body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -85,3 +86,27 @@ async def read_user_item(
             {"description": "This is an amazing item that has a long description"}
         )
     return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# request body
+{
+    "name": "abcd",
+    "description": "i am abcd",
+    "price": 1000,
+    "tax": 10
+}
