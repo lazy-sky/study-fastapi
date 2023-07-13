1447f4b59e55a420200c028211093a23c6e6f05d
lazy-sky <1cielo@naver.com>
2023-07-13 16:14:15 +0900

    Cookie Parameters

--- main.py
+++ main.py
@@ -1,31 +1,12 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, Field
+from typing import Annotated
+
+from fastapi import Cookie, FastAPI
 
 app = FastAPI()
 
-
-class Item(BaseModel):
-    name: str = Field(examples=["Foo"])
-    description: str | None = Field(
-        default=None, examples=["A very nice Item"])
-    price: float = Field(examples=[35.4])
-    tax: float | None = Field(default=None, examples=[3.2])
-
-    # model_config = {
-    #     "json_schema_extra": {
-    #         "examples": [
-    #             {
-    #                 "name": "Foo",
-    #                 "description": "A very nice Item",
-    #                 "price": 35.4,
-    #                 "tax": 3.2,
-    #             }
-    #         ]
-    #     }
-    # }
+# Query, Path 파라미터와 같은 방식으 쿠키 파라미터 정의 가능
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-    return results
+@app.get("/items/")
+async def read_items(ads_id: Annotated[str | None, Cookie()] = None):
+    return {"ads_id": ads_id}
