d3f3da774d1df5a2b96eeb341d86f4b032a2a239
lazy-sky <1cielo@naver.com>
2023-07-13 16:07:23 +0900

    Declare Request Example Data

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-from typing import Annotated
-
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
-    name: str
+    name: str = Field(examples=["Foo"])
     description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(
-        gt=0, description="The price must be greater than zero")
-    tax: float | None = None
+        default=None, examples=["A very nice Item"])
+    price: float = Field(examples=[35.4])
+    tax: float | None = Field(default=None, examples=[3.2])
+
+    # model_config = {
+    #     "json_schema_extra": {
+    #         "examples": [
+    #             {
+    #                 "name": "Foo",
+    #                 "description": "A very nice Item",
+    #                 "price": 35.4,
+    #                 "tax": 3.2,
+    #             }
+    #         ]
+    #     }
+    # }
 
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
